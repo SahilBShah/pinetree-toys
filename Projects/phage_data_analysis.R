fc43ac6b2900b0563fe62e021fb816f0aa915c44
Sahil-S10 <sahil2699@gmail.com>
2019-06-11 13:58:44 -0500

    Started evolutionary program associated with three genes model

--- Projects/phage_data_analysis.R
+++ Projects/phage_data_analysis.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(readr)
 
 setwd("/home/wilkelab/pinetree-toys/Projects/phage_simulation-master/data/simulation/phage/")
-phage_data <- read_tsv('phage_degrade_recoded_27_counts.tsv')
+phage_data <- read_tsv('phage_wildtype.tsv')
 phage_data <- dplyr::filter(phage_data, grepl('gene', species))
 phage_data <- dplyr::filter(phage_data, !grepl('rbs', species))
 phage_bar_plot <- ggplot(phage_data, aes(fill=species, x=species, y=transcript)) + geom_bar(stat="identity")
