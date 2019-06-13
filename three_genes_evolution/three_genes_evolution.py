48e0b26c36ee36ae4cad6055f6776022122a74fa
Sahil Shah <sahil2699@gmail.com>
2019-06-13 00:52:52 -0500

    Added features to evolution program

--- three_genes_evolution/three_genes_evolution.py
+++ three_genes_evolution/three_genes_evolution.py
@@ -2,19 +2,22 @@ import pinetree as pt
 import pandas
 import random
 import numpy as np
+import string
 
 #Taking in target tsv file
 #df = pandas.read_csv(input("Enter .tsv file name: "), sep="/t", engine="python")
 df = pandas.read_csv("three_genes_rnase.tsv", sep="/t", engine="python")
 
 def main():
+
     f_old = 1.0
+    mu = 0
     sigma = 1
-    eps = random.randint(0,5) / 100 * sigma
-    Ne = 5
+    Ne = 10
     i = 0
+
     #Creating starting three genes sequence
-    sim = pt.Model(cell_volume=8e-16)
+    '''sim = pt.Model(cell_volume=8e-16)
     sim.seed(34)
     sim.add_polymerase(name="rnapol", copy_number=4, speed=40, footprint=10)
     sim.add_ribosome(copy_number=100, speed=30, footprint=10)
@@ -27,7 +30,7 @@ def main():
 
     # plasmid = pt.Genome(name="plasmid", length=300)
 
-    prom = plasmid.add_promoter(name="p1", start=1, stop=10,
+    plasmid.add_promoter(name="p1", start=1, stop=10,
                          interactions={"rnapol": 4e10})
     plasmid.add_terminator(name="t1", start=449, stop=450,
                            efficiency={"rnapol": 1.0})
@@ -40,33 +43,64 @@ def main():
 
     plasmid.add_gene(name="proteinZ", start=165 + 150, stop=298 + 150,
                      rbs_start=(165 - 15 + 150), rbs_stop=165 + 150, rbs_strength=1e7)
+    sim.register_genome(plasmid)
+    sim.simulate(time_limit=240, time_step=1,
+                 output = "three_genes_rnase2.tsv")'''
 
-    while i < 10:
-        eps = random.randint(0,5) / 100 * sigma
+    #Evolution program - auto changing polymerase strength
+    prom = 0
+    while i < 50:
+        name = name2 = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+        vars()[name] = pt.Genome(name="plasmid", length=450,
+                            transcript_degradation_rate=1e-2,
+                            transcript_degradation_rate_ext=1e-2,
+                            rnase_speed=20,
+                            rnase_footprint=10)
+        eps = np.random.normal(mu, sigma)
         f_new = f_old * (1.0 + eps)
         new_pol_strength = random.randint(1e10, 5e10)
-        #calculate fitness of new mutation
-        probability = calc_fitness(f_new, f_old, Ne)
-        f_old = probability
-        print("Polymerase Strength = ", new_pol_strength)
         if f_new > f_old:
-            prom = plasmid.add_promoter(name="p1", start=1, stop=10,
+            #Accepting mutation
+            prom = vars()[name].add_promoter(name="p1", start=1, stop=10,
                                  interactions={"rnapol": new_pol_strength})
         else:
+            #Calculate fitness of new mutation
             probability = calc_fitness(f_new, f_old, Ne)
             f_old = probability
             if probability > random.random():
-                prom = plasmid.add_promoter(name="p1", start=1, stop=10,
+                #Accepting mutation
+                prom = vars()[name].add_promoter(name="p1", start=1, stop=10,
                                      interactions={"rnapol": new_pol_strength})
-        #Add check equivalence bw tsv files feature
-        print("f_new = ", f_new)
-        print("f_old = ", f_old)
+
+        #recreated_genome(prom, name2)
+        #Recreated three genome sequence in order to simulate in loop
+        sim = pt.Model(cell_volume=8e-16)
+        sim.seed(34)
+        sim.add_polymerase(name="rnapol", copy_number=4, speed=40, footprint=10)
+        sim.add_ribosome(copy_number=100, speed=30, footprint=10)
+
+        vars()[name] = pt.Genome(name="plasmid", length=450,
+                            transcript_degradation_rate=1e-2,
+                            transcript_degradation_rate_ext=1e-2,
+                            rnase_speed=20,
+                            rnase_footprint=10)
         prom
-        sim.register_genome(plasmid)
+        vars()[name].add_terminator(name="t1", start=449, stop=450,
+                               efficiency={"rnapol": 1.0})
+
+        vars()[name].add_gene(name="proteinX", start=26, stop=148,
+                         rbs_start=(26 - 15), rbs_stop=26, rbs_strength=1e7)
+
+        vars()[name].add_gene(name="proteinY", start=26 + 150, stop=148 + 150,
+                         rbs_start=(26 - 15 + 150), rbs_stop=26 + 150, rbs_strength=1e7)
+
+        vars()[name].add_gene(name="proteinZ", start=165 + 150, stop=298 + 150,
+                         rbs_start=(165 - 15 + 150), rbs_stop=165 + 150, rbs_strength=1e7)
+        sim.register_genome(vars()[name])
         sim.simulate(time_limit=240, time_step=1,
-                     output="three_genes_rnase2.tsv")
-        nf = pandas.read_csv("three_genes_rnase2.tsv", sep="/t", engine="python")
-        check_files(df, nf)
+                     output = "three_genes_rnase2.tsv")
+        #nf = pandas.read_csv("three_genes_rnase2.tsv", sep="/t", engine="python")
+        #check_files(df, nf)
         i+=1
 
 #Calculates the fitness of the new mutation
@@ -99,6 +133,33 @@ def check_files(target_file, new_file):
             elif(row2[3] - row[3]) == 0:
                 check_files(target_file, new_file)
                 break'''
+
+'''def recreated_genome(promoter, name):
+    sim = pt.Model(cell_volume=8e-16)
+    sim.seed(34)
+    sim.add_polymerase(name="rnapol", copy_number=4, speed=40, footprint=10)
+    sim.add_ribosome(copy_number=100, speed=30, footprint=10)
+
+    name = pt.Genome(name="plasmid", length=450,
+                        transcript_degradation_rate=1e-2,
+                        transcript_degradation_rate_ext=1e-2,
+                        rnase_speed=20,
+                        rnase_footprint=10)
+    promoter
+    name.add_terminator(name="t1", start=449, stop=450,
+                           efficiency={"rnapol": 1.0})
+
+    name.add_gene(name="proteinX", start=26, stop=148,
+                     rbs_start=(26 - 15), rbs_stop=26, rbs_strength=1e7)
+
+    name.add_gene(name="proteinY", start=26 + 150, stop=148 + 150,
+                     rbs_start=(26 - 15 + 150), rbs_stop=26 + 150, rbs_strength=1e7)
+
+    name.add_gene(name="proteinZ", start=165 + 150, stop=298 + 150,
+                     rbs_start=(165 - 15 + 150), rbs_stop=165 + 150, rbs_strength=1e7)
+    sim.register_genome(name)
+    sim.simulate(time_limit=240, time_step=1,
+                 output = "three_genes_rnase2.tsv")'''
 
 
 
