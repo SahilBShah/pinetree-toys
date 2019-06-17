fa35b6a1adb2208e8dab8bcaeada13d2255172b9
Sahil-S10 <sahil2699@gmail.com>
2019-06-17 14:53:24 -0500

    Added second variable to evolution program and output files function but needs work on optimization

--- three_genes_evolution/three_genes_evolution.py
+++ three_genes_evolution/three_genes_evolution.py
@@ -10,76 +10,61 @@ def main():
     global poly_list
     global all_poly_list
     global sos_list
+    global all_sos_list
+    global term_list
+    global all_term_list
     sos_list = [60000]
     poly_list = []
     all_poly_list = []
+    all_sos_list = []
+    term_list = []
+    all_term_list = []
     f_old = 1.0
     mu = 0.0
     sigma = 1.0
+    poly_sigma = 1e10
+    term_sigma = 1.0
     Ne = 10
     i = 0
     gen = 0
 
     #Taking in target tsv file
-    df = pandas.read_table("three_genes_rnase.tsv", delim_whitespace=True, header=0)
+    df = pandas.read_table("three_genes_test_file.tsv", delim_whitespace=True, header=0)
     df = edit_target_file(df)
+    aspect_probability = random.uniform(0.0, 1.0)
 
-    #Creating starting three genes sequence
-    sim = pt.Model(cell_volume=8e-16)
-    sim.seed(34)
-    sim.add_polymerase(name="rnapol", copy_number=4, speed=40, footprint=10)
-    sim.add_ribosome(copy_number=100, speed=30, footprint=10)
-
-    plasmid = pt.Genome(name="plasmid", length=450,
-                        transcript_degradation_rate=1e-2,
-                        transcript_degradation_rate_ext=1e-2,
-                        rnase_speed=20,
-                        rnase_footprint=10)
-
-    # plasmid = pt.Genome(name="plasmid", length=300)
-
-    plasmid.add_promoter(name="p1", start=1, stop=10,
-                         interactions={"rnapol": 4e10})
-    plasmid.add_terminator(name="t1", start=449, stop=450,
-                           efficiency={"rnapol": 1.0})
-
-    plasmid.add_gene(name="proteinX", start=26, stop=148,
-                     rbs_start=(26 - 15), rbs_stop=26, rbs_strength=1e7)
-
-    plasmid.add_gene(name="proteinY", start=26 + 150, stop=148 + 150,
-                     rbs_start=(26 - 15 + 150), rbs_stop=26 + 150, rbs_strength=1e7)
-
-    plasmid.add_gene(name="proteinZ", start=165 + 150, stop=298 + 150,
-                     rbs_start=(165 - 15 + 150), rbs_stop=165 + 150, rbs_strength=1e7)
-    sim.register_genome(plasmid)
-    sim.simulate(time_limit=240, time_step=1,
-                 output = "three_genes_rnase2.tsv")
-
-    #Evolution program - auto changing polymerase strength
+    #Evolution program
     new_pol_strength = random.randint(1e10, 5e10)
+    new_term_efficiency = random.randint(1, 10)
     poly_list.append(new_pol_strength)
     all_poly_list.append(new_pol_strength)
-    while i < 1000:
+    term_list.append(new_term_efficiency)
+    all_term_list.append(new_term_efficiency)
+    while i < 100:
+        aspect_probability = random.uniform(0.0, 1.0)
+        #Evolution program - auto changing polymerase strength
+        #if aspect_probability > 0.5:
         eps = np.random.normal(mu, sigma)
+        poly_eps = np.random.normal(mu, poly_sigma)
         f_new = f_old * (1.0 + eps)
-        #new_pol_strength = random.randint(1e10, 5e10)
 
         if f_new > f_old:
             #Accepting mutation
             three_genome.recreated_genome(new_pol_strength)
             #Taking in new file and removing unnecessary rows and columns
-            nf = pandas.read_table("three_genes_rnase2.tsv", delim_whitespace=True, header=0)
+            nf = pandas.read_table("three_genes_replicated.tsv", delim_whitespace=True, header=0)
             nf = edit_new_file(nf)
             sos = sum_of_squares(df, nf)
+            all_sos_list.append(sos)
+            if sos == 0:
+                break
             #Accepts mutation only if sum of squares value decreases
             if sos > sos_list[-1]:
                 three_genome.recreated_genome(poly_list[-1])
                 sos = sum_of_squares(df, nf)
-            if sos < sos_list[-1]:
+            if sos <= sos_list[-1]:
                 poly_list.append(new_pol_strength)
                 sos_list.append(sos)
-            print("Poly List = ", poly_list)
-            print("SOS List = ", sos_list)
             gen+=1
         else:
             #Calculate fitness of new mutation
@@ -89,27 +74,108 @@ def main():
                 #Accepting mutation
                 three_genome.recreated_genome(new_pol_strength)
                 #Taking in new file and removing unnecessary rows and columns
-                nf = pandas.read_table("three_genes_rnase2.tsv", delim_whitespace=True, header=0)
+                nf = pandas.read_table("three_genes_replicated.tsv", delim_whitespace=True, header=0)
                 nf = edit_new_file(nf)
                 sos = sum_of_squares(df, nf)
+                all_sos_list.append(sos)
+                if sos == 0:
+                    break
                 #Accepts mutation only if sum of squares value decreases
                 if sos > sos_list[-1]:
                     three_genome.recreated_genome(poly_list[-1])
                     sos = sum_of_squares(df, nf)
-                if sos < sos_list[-1]:
+                if sos <= sos_list[-1]:
                     poly_list.append(new_pol_strength)
                     sos_list.append(sos)
-                    new_pol_strength = random.randint(poly_list[-1], poly_list[-2])
-                print("Poly List = ", poly_list)
-                print("SOS List = ", sos_list)
                 gen+=1
 
         #Determines new polymerase strength to reduce sum of squares value
-        all_poly_list.append(new_pol_strength)
-        new_pol_strength = random.randint(1e10, poly_list[-1])
+        new_pol_strength = poly_list[-1] + poly_eps
+        #if sos_list[-1] < 3000:
+            #poly_sigma = 1e7
+        while new_pol_strength < 0:
+            poly_eps = np.random.normal(mu, poly_sigma)
+            new_pol_strength = poly_list[-1] + poly_eps
+            if new_pol_strength in all_poly_list:
+                poly_eps = np.random.normal(mu, poly_sigma)
+                new_pol_strength = poly_list[-1] + poly_eps
         if new_pol_strength in all_poly_list:
-            new_pol_strength = random.randint(1e10, poly_list[-1])
+            poly_eps = np.random.normal(mu, poly_sigma)
+            new_pol_strength = poly_list[-1] + poly_eps
+        all_poly_list.append(new_pol_strength)
+
+        #Evolution program - auto changing terminator efficiency rate
+        '''if aspect_probability < 0.5:
+            eps = np.random.normal(mu, sigma)
+            term_eps = np.random.normal(mu, term_sigma)
+            f_new = f_old * (1.0 + eps)
+
+            if f_new > f_old:
+                #Accepting mutation
+                three_genome.recreated_genome(new_pol_strength, new_term_efficiency)
+                #Taking in new file and removing unnecessary rows and columns
+                nf = pandas.read_table("three_genes_replicated.tsv", delim_whitespace=True, header=0)
+                nf = edit_new_file(nf)
+                sos = sum_of_squares(df, nf)
+                all_sos_list.append(sos)
+                if sos == 0:
+                    break
+                #Accepts mutation only if sum of squares value decreases
+                if sos > sos_list[-1]:
+                    three_genome.recreated_genome(poly_list[-1], term_list[-1])
+                    sos = sum_of_squares(df, nf)
+                if sos <= sos_list[-1]:
+                    term_list.append(new_term_efficiency)
+                    sos_list.append(sos)
+                gen+=1
+            else:
+                #Calculate fitness of new mutation
+                probability = calc_fitness(f_new, f_old, Ne)
+                f_old = probability
+                if probability > random.random():
+                    #Accepting mutation
+                    three_genome.recreated_genome(new_pol_strengt, new_term_efficiency)
+                    #Taking in new file and removing unnecessary rows and columns
+                    nf = pandas.read_table("three_genes_replicated.tsv", delim_whitespace=True, header=0)
+                    nf = edit_new_file(nf)
+                    sos = sum_of_squares(df, nf)
+                    all_sos_list.append(sos)
+                    if sos == 0:
+                        break
+                    #Accepts mutation only if sum of squares value decreases
+                    if sos > sos_list[-1]:
+                        three_genome.recreated_genome(poly_list[-1], term_list[-1])
+                        sos = sum_of_squares(df, nf)
+                    if sos <= sos_list[-1]:
+                        term_list.append(new_term_efficiency)
+                        sos_list.append(sos)
+                    gen+=1
+
+            #Determines new terminator efficiency value to reduce sum of squares value
+            new_term_efficiency = term_list[-1] + term_eps
+            while term_list < 0:
+                term_eps = np.random.normal(mu, term_sigma)
+                new_term_efficiency = term_list[-1] + term_eps
+                if new_term_efficiency in all_term_list:
+                    term_eps = np.random.normal(mu, term_sigma)
+                    new_term_efficiency = term_list[-1] + term_eps
+            if new_term_efficiency in all_term_list:
+                term_eps = np.random.normal(mu, term_sigma)
+                new_term_efficiency = term_list[-1] + term_eps
+            all_term_list.append(new_term_efficiency)'''
+
         i+=1
+
+    #Export tsv file of sum of squares and polymerase rate data
+    all_sos_dataframe = pandas.DataFrame(all_sos_list, columns=["Sum of Squares"])
+    export_csv = all_sos_dataframe.to_csv("~/pinetree-toys/three_genes_evolution/all_sos_data.tsv", index=False)
+    sos_dataframe = pandas.DataFrame(sos_list, columns=["Sum of Squares"])
+    export_csv = sos_dataframe.to_csv("~/pinetree-toys/three_genes_evolution/sos_data.tsv", index=False)
+    all_poly_dataframe = pandas.DataFrame(all_poly_list, columns=["Polymerase Rate"])
+    export_csv = all_poly_dataframe.to_csv("~/pinetree-toys/three_genes_evolution/all_poly_data.tsv", index=False)
+    poly_dataframe = pandas.DataFrame(poly_list, columns=["Polymerase Rate"])
+    export_csv = poly_dataframe.to_csv("~/pinetree-toys/three_genes_evolution/poly_data.tsv", index=False)
+    print("Final Polymerase Rate: ", poly_list[-1])
     print("Generations = ", gen)
 
 #Calculates the fitness of the new mutation
@@ -120,7 +186,7 @@ def calc_fitness(variant_fit, orig_fit, Ne):
     N = Ne
 
     if xj == xi:
-        return(1.0 / float(N))
+        return(1.0 / float(N) )
     if xj > xi:
         return(1.0)
     else:
@@ -128,7 +194,7 @@ def calc_fitness(variant_fit, orig_fit, Ne):
         return variant_fit
 
     try:
-        p =((1-pow( (xi/xj),2)) /(1-pow( (xi/xj), (2 * float(N) )) ) )
+        p =((1-pow((xi/xj), 2)) /(1-pow((xi/xj), (2 * float(N) )) ) )
     except OverflowError as e:
         p = 0.0
     return (p)
@@ -208,7 +274,7 @@ class three_genome:
                          rbs_start=(165 - 15 + 150), rbs_stop=165 + 150, rbs_strength=1e7)
         sim.register_genome(plasmid)
         sim.simulate(time_limit=240, time_step=1,
-                     output = "three_genes_rnase2.tsv")
+                     output = "three_genes_replicated.tsv")
 
 
 
