b6f8e7b50aab5964c3df942ffeee8092d16152d4
Sahil-S10 <sahil2699@gmail.com>
2019-08-05 17:36:15 -0500

    Memory leak tests

---
+++ three_genes_evolution/memory_leak_tests/os_orig_genome.py
@@ -0,0 +1,5 @@
+import os
+
+class call_pt:
+    def pt_call(self):
+        os.system('~/pinetree_test.py')
---
+++ three_genes_evolution/memory_leak_tests/pt_call.py
@@ -0,0 +1,9 @@
+from os_orig_genome import call_pt
+import os
+
+i = 1
+x = os.system('~/pinetree_test.py')
+
+for i in range(100000):
+    x
+    print("i =", i)
