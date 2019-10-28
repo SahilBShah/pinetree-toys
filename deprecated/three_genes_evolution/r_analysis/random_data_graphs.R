200b5567337fbdb564dabee15541d5eae8995eb5
Sahil Shah <sahil2699@gmail.com>
2019-10-28 08:42:02 -0500

    Adding more target files to run on TACC

--- deprecated/three_genes_evolution/r_analysis/random_data_graphs.R
+++ deprecated/three_genes_evolution/r_analysis/random_data_graphs.R
@@ -4,15 +4,15 @@ library(gridExtra)
 library(ggplot2)
 library(gtools)
 
-setwd("/home/sahil/pinetree-toys/three_genes_evolution/final_program/")
+setwd("/home/sahil/pinetree-toys/data/grant_data/")
 
 
 
 
 
-original_data <- read.table("random_data5.tsv", header=TRUE)
+original_data <- read.table("grant_data13.tsv", header=TRUE)
 original_data <- filter(original_data, species == "proteinX" | species == "proteinY" | species == "proteinZ")
 original_line_plot <-  ggplot(original_data, aes(fill=species, color=species, x=time, y=transcript)) + geom_line(stat="identity") + ggtitle("Original Genome Function") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.line = element_line(colour = "black"))
 
 gen_plots <- grid.arrange(original_line_plot, nrow=1, ncol=1)
-ggsave("/home/sahil/pinetree-toys/three_genes_evolution/final_program/random_data5_graph.jpg", gen_plots, width=20, height=10)
+ggsave("/home/sahil/pinetree-toys/data/grant_data/grant_data13.pdf", gen_plots, width=20, height=10)
