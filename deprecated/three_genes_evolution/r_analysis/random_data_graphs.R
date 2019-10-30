4bd5df0e3479b3d274eaa5b7681818b2bbf16749
Sahil Shah <sahil2699@gmail.com>
2019-10-30 15:48:54 -0500

    Remaking grant data 1 graph

--- deprecated/three_genes_evolution/r_analysis/random_data_graphs.R
+++ deprecated/three_genes_evolution/r_analysis/random_data_graphs.R
@@ -10,9 +10,9 @@ setwd("/home/sahil/pinetree-toys/data/grant_data/")
 
 
 
-original_data <- read.table("grant_data13.tsv", header=TRUE)
+original_data <- read.table("grant_data1.tsv", header=TRUE)
 original_data <- filter(original_data, species == "proteinX" | species == "proteinY" | species == "proteinZ")
 original_line_plot <-  ggplot(original_data, aes(fill=species, color=species, x=time, y=transcript)) + geom_line(stat="identity") + ggtitle("Original Genome Function") + theme(panel.grid.major = element_blank(), panel.grid.minor = element_blank(), panel.background = element_blank(), axis.line = element_line(colour = "black"))
 
 gen_plots <- grid.arrange(original_line_plot, nrow=1, ncol=1)
-ggsave("/home/sahil/pinetree-toys/data/grant_data/grant_data13.pdf", gen_plots, width=20, height=10)
+ggsave("/home/sahil/pinetree-toys/data/grant_data/grant_data1.pdf", gen_plots, width=20, height=10)
